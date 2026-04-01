95303a9b0bf90ca68c10e1aa46d47cc8088650aa
CePseudo <hoffmann.axel.6032@gmail.com>
2026-04-01 19:24:54 +0200

    chore: add dependencies, critical CSS inlining and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import critters from 'astro-critters';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [critters()],
+});
